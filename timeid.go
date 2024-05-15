0f049d3cc8f4aaec895068df4f36dfbc22874e8a
Mark Karpeles <magicaltux@gmail.com>
2024-05-15 16:47:27 +0900

    timeid unique/etc

--- timeid.go
+++ timeid.go
@@ -2,6 +2,7 @@ package ellipxobj
 
 import (
 	"encoding/binary"
+	"fmt"
 	"time"
 )
 
@@ -11,8 +12,35 @@ type TimeId struct {
 	Index uint32 `json:"idx"`  // index if multiple ids are generated with the same unix/nano values
 }
 
+type TimeIdUnique struct {
+	Last TimeId
+}
+
+var uniqueTime TimeIdUnique
+
+// NewTimeId returns a new TimeId for now
+func NewTimeId() *TimeId {
+	t := time.Now()
+	res := &TimeId{
+		Unix: uint64(t.Unix()),
+		Nano: uint32(t.Nanosecond()),
+	}
+	return res
+}
+
+// NewUniqueTimeId returns a unique (in the local process) [TimeId]
+func NewUniqueTimeId() *TimeId {
+	t := NewTimeId()
+	uniqueTime.Unique(t)
+	return t
+}
+
 func (t TimeId) Time() time.Time {
 	return time.Unix(int64(t.Unix), int64(t.Nano))
+}
+
+func (t TimeId) String() string {
+	return fmt.Sprintf("%d:%d:%d", t.Unix, t.Nano, t.Index)
 }
 
 // Bytes returns a 128bits bigendian sortable version of this TimeId
@@ -23,4 +51,29 @@ func (t TimeId) Bytes() []byte {
 	binary.BigEndian.PutUint32(res[12:], t.Index)
 
 	return res
+}
+
+// Unique ensures the provided [TimeId] is always higher than the latest
+// one and will update it if not the case
+func (u *TimeIdUnique) Unique(t *TimeId) {
+	if t.Unix > u.Last.Unix {
+		u.Last = *t
+		return
+	}
+	if t.Unix == u.Last.Unix {
+		if t.Nano > u.Last.Nano {
+			u.Last = *t
+			return
+		}
+		if t.Nano == u.Last.Nano {
+			if t.Index > u.Last.Index {
+				u.Last.Index = t.Index
+				return
+			}
+		}
+	}
+
+	// re-use last
+	u.Last.Index += 1
+	*t = u.Last
 }
