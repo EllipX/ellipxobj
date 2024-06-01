2abfa91242dfa16f198bd07549b5fa691b1d829d
Mark Karpeles <magicaltux@gmail.com>
2024-06-01 19:47:34 +0900

    add flexible unmarshal for pairs

--- pair.go
+++ pair.go
@@ -1,5 +1,11 @@
 package ellipxobj
 
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
 type PairName [2]string
 
 func Pair(a, b string) PairName {
@@ -8,4 +14,39 @@ func Pair(a, b string) PairName {
 
 func (p PairName) String() string {
 	return p[0] + "_" + p[1]
+}
+
+func (p *PairName) UnmarshalJSON(v []byte) error {
+	// can be either a string or an array of two strings
+	if string(v) == "null" {
+		// do nothing
+		return nil
+	}
+
+	switch v[0] {
+	case '[':
+		var t [2]string
+		err := json.Unmarshal(v, &t)
+		if err != nil {
+			return err
+		}
+		*p = PairName(t)
+		return nil
+	case '"':
+		var t string
+		err := json.Unmarshal(v, &t)
+		if err != nil {
+			return err
+		}
+		// must contain a _
+		pos := strings.IndexByte(t, '_')
+		if pos == -1 {
+			return errors.New("malformed pair")
+		}
+		(*p)[0] = t[:pos]
+		(*p)[1] = t[pos+1:]
+		return nil
+	default:
+		return errors.New("cannot unmarshal json into pair")
+	}
 }
