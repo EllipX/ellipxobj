7b098a3fcfe273c7895b24d1825dd4c96773e176
Mark Karpeles <magicaltux@gmail.com>
2024-06-05 12:27:26 +0900

    tweak trade

--- trade.go
+++ trade.go
@@ -2,11 +2,17 @@ package ellipxobj
 
 // Trade represents a trade that happened, where two orders matched
 type Trade struct {
-	Id     *TimeId // trade id
-	Pair   PairName
-	Bid    *OrderMeta
-	Ask    *OrderMeta
-	Type   OrderType // taker's order type
-	Amount *Amount
-	Price  *Amount
+	Id     *TimeId    `json:"id"` // trade id
+	Pair   PairName   `json:"pair"`
+	Bid    *OrderMeta `json:"bid"`
+	Ask    *OrderMeta `json:"ask"`
+	Type   OrderType  `json:"type"` // taker's order type
+	Amount *Amount    `json:"amount"`
+	Price  *Amount    `json:"price"`
+}
+
+// Spent returns the amount spent in that trade
+func (t *Trade) Spent() *Amount {
+	c, _ := NewAmount(0, t.Price.exp).Mul(t.Amount, t.Price)
+	return c
 }
