f77a70cf679c3a9e8399232f9eca93d53a43cdfa
Mark Karpeles <magicaltux@gmail.com>
2024-08-21 18:18:37 +0900

    add date to marshalled trade info

--- trade.go
+++ trade.go
@@ -1,5 +1,10 @@
 package ellipxobj
 
+import (
+	"encoding/json"
+	"time"
+)
+
 // Trade represents a trade that happened, where two orders matched
 type Trade struct {
 	Id     *TimeId    `json:"id"` // trade id
@@ -9,6 +14,33 @@ type Trade struct {
 	Type   OrderType  `json:"type"` // taker's order type
 	Amount *Amount    `json:"amount"`
 	Price  *Amount    `json:"price"`
+}
+
+// tradeMarshalled is the representation of Trade when marshalled as JSON
+type tradeMarshalled struct {
+	Id     *TimeId    `json:"id"` // trade id
+	Pair   PairName   `json:"pair"`
+	Bid    *OrderMeta `json:"bid"`
+	Ask    *OrderMeta `json:"ask"`
+	Type   OrderType  `json:"type"` // taker's order type
+	Amount *Amount    `json:"amount"`
+	Price  *Amount    `json:"price"`
+	Date   time.Time  `json:"date"`
+}
+
+func (t *Trade) MarshalJSON() ([]byte, error) {
+	obj := &tradeMarshalled{
+		Id:     t.Id,
+		Pair:   t.Pair,
+		Bid:    t.Bid,
+		Ask:    t.Ask,
+		Type:   t.Type,
+		Amount: t.Amount,
+		Price:  t.Price,
+		Date:   t.Id.Time(),
+	}
+
+	return json.Marshal(obj)
 }
 
 // Spent returns the amount spent in that trade
