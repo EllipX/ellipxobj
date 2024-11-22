0846bac71abcf51a8ca0b1f9200cbb7212c0db73
Mark Karpeles <magicaltux@gmail.com>
2024-11-22 15:21:17 +0900

    add point to checkpoint

--- checkpoint.go
+++ checkpoint.go
@@ -5,6 +5,7 @@ type Checkpoint struct {
 	Epoch      uint64   `json:"epoch"`
 	PrevEpoch  uint64   `json:"prev"`
 	PrevHash   []byte   `json:"prev_hash"`
+	Point      TimeId   `json:"point"`
 	OrderSum   []byte   `json:"in_sum"`
 	OrderCount uint64   `json:"in_cnt"`
 	Bids       []*Order `json:"bids"`
